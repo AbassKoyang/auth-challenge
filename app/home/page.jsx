be5fe4c3c52e27e6fa271faf29eadfe930529235
Abass Koyang <abasskoyang12345@gmail.com>
2023-10-08 05:33:41 +0930

    added callback url to sign out button

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { signIn, signOut, useSession, getProviders, SessionProvider} from 'next-auth/react';
+import { useState, useEffect } from 'react';
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+    <button type='button' onClick={() => signOut({callbackUrl: "http://localhost:3000/login"})}>Sign Out Now</button>
+    </div>
+
   )
 }
 
